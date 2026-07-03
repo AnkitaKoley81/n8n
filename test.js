3c5173f8ff517bf0a4a120104969a97dd739cdc8
AnkitaKoley81 <ankitakoley81@gmail.com>
2026-07-03 18:01:54 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,28 @@
-cons a = 7;
-const b = 6;
+// test.js
 
-console.log(a)
-console.log(b)
+console.log("Hello, GitHub!");
+
+function greet(name) {
+    return `Hello, ${name}! Welcome to JavaScript.`;
+}
+
+const user = "Ankita";
+
+console.log(greet(user));
+
+// Array example
+const numbers = [10, 20, 30, 40, 50];
+
+const sum = numbers.reduce((total, num) => total + num, 0);
+
+console.log("Numbers:", numbers);
+console.log("Sum:", sum);
+
+// Object example
+const student = {
+    name: "Ankita",
+    course: "JavaScript",
+    completed: false
+};
+
+console.log(student);
